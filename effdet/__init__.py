493d145100565ffe5f8ae2918243c9cf87c8da9a
adypd97 <66629616+adypd97@users.noreply.github.com>
2021-07-23 16:37:56 +0530

    allow importing set_config_writeable

--- effdet/__init__.py
+++ effdet/__init__.py
@@ -2,6 +2,6 @@ from .efficientdet import EfficientDet
 from .bench import DetBenchPredict, DetBenchTrain, unwrap_bench
 from .data import create_dataset, create_loader, create_parser, DetectionDatset, SkipSubset
 from .evaluator import CocoEvaluator, PascalEvaluator, OpenImagesEvaluator, create_evaluator
-from .config import get_efficientdet_config, default_detection_model_configs
+from .config import get_efficientdet_config, default_detection_model_configs, set_config_writeable
 from .factory import create_model, create_model_from_config
 from .helpers import load_checkpoint, load_pretrained
